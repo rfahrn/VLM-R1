9c2682606860d44a00effb8c7c3baa8bc0cc14a9
Rebecka <58358083+rfahrn@users.noreply.github.com>
2025-06-21 13:16:25 +0200

    Update experiments.sh

--- run_scripts/experiments.sh
+++ run_scripts/experiments.sh
@@ -5,6 +5,8 @@
 
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 export REPO_HOME="${PROJECT_ROOT}"
+source ~/.bashrc
+conda activate rebecka
 
 # Your CXR data
 data_paths="${HOME}/train_scxr2.jsonl"
