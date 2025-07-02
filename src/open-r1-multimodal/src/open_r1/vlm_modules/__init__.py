68b906b6ca10783dc541fcc302f5dc169ac5a42a
Rebecka <58358083+rfahrn@users.noreply.github.com>
2025-07-02 11:53:05 +0200

    Update __init__.py

--- src/open-r1-multimodal/src/open_r1/vlm_modules/__init__.py
+++ src/open-r1-multimodal/src/open_r1/vlm_modules/__init__.py
@@ -1,5 +1,6 @@
 from .vlm_module import VLMBaseModule
 from .qwen_module import Qwen2VLModule
 from .internvl_module import InvernVLModule
+from .llava_module import LlavaOneVisionModule 
 
-__all__ = ["VLMBaseModule", "Qwen2VLModule", "InvernVLModule"]
+__all__ = ["VLMBaseModule", "Qwen2VLModule", "InvernVLModule", "LlavaOneVisionModule"]
